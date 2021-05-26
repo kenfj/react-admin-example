c521d03227ecedfb59a6c851f3a1931635caa0a8
Ken Fujii <ken.fj@icloud.com>
2021-05-26 22:29:20 +0900

    add github corner

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import PeopleIcon from '@material-ui/icons/People';
 import jsonServerProvider from 'ra-data-json-server';
 import React, { useState } from 'react';
 import { Admin, DataProvider, Resource } from 'react-admin';
+import GithubCorner from 'react-github-corner';
 import Dashboard from './dashboard/Dashboard';
 import PostList from './posts/PostList';
 import PostShow from './posts/PostShow';
@@ -20,12 +21,18 @@ function App() {
   // ListGuesser EditGuesser ShowGuesser are available
   // Note: ShowGuesser need /show in the URL
   // https://github.com/marmelab/react-admin/issues/4833
+
+  const sourceUrl = 'https://github.com/kenfj/react-admin-example'
+
   return (
-    <Admin dataProvider={dataProvider} dashboard={Dashboard}>
-      <Resource name="users" list={UserList} icon={PeopleIcon} show={UserShow} options={{ label: 'User List' }} />
-      <Resource name="posts" list={PostList} icon={MessageIcon} show={PostShow} />
-      <Resource name="todos" list={TodoList} icon={ListIcon} />
-    </Admin>
+    <>
+      <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+        <Resource name="users" list={UserList} icon={PeopleIcon} show={UserShow} options={{ label: 'User List' }} />
+        <Resource name="posts" list={PostList} icon={MessageIcon} show={PostShow} />
+        <Resource name="todos" list={TodoList} icon={ListIcon} />
+      </Admin>
+      <GithubCorner href={sourceUrl} size={45} />
+    </>
   )
 }
 
